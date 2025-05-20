a335f156c9e097d7ebb517cf75b6782cba5cf000
lerquor <lermurodeng@gmail.com>
2025-05-20 13:52:32 +0300

    first cs homework

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please, write the first number");
+        if (!int.TryParse(Console.ReadLine(), out int a))
+        {
+            Console.WriteLine("Error!");
+            return;
+        }
+        Console.WriteLine("Please, write the second number");
+
+        if (!int.TryParse(Console.ReadLine(), out int b))
+        {
+            Console.WriteLine("Error!");
+            return;
+        }
+        var sign = Console.ReadLine();
+        var number = 0;
+
+        if (sign.Length > 1 || sign.Length == 0)
+        {
+            Console.WriteLine("Wrong sign");
+            return;
+        }
+        switch (sign[0])
+        {
+            case '|':
+                number = a | b;
+                Console.WriteLine(number + " " + Convert.ToString(number, 2) + " " + Convert.ToString(number, 16));
+                break;
+            case '&':
+                number = a & b;
+                Console.WriteLine(number + " " + Convert.ToString(number, 2) + " " + Convert.ToString(number, 16));
+                break;
+            case '^':
+                number = a ^ b;
+                Console.WriteLine(number + " " + Convert.ToString(number, 2) + " " + Convert.ToString(number, 16));
+                break;
+            default: 
+                Console.WriteLine("Wrong sign!");
+                break;
+        }
+    }
+}
