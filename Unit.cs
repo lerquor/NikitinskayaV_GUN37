03168b0fbe19c0caab5950c32ecf8419c2bb0dcd
lerquor <lermurodeng@gmail.com>
2025-06-20 12:56:24 +0300

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Unit
+    {
+        private float _health;
+
+        public string Name { get; }
+        public float Health => _health;
+
+        private Interval Damage { get; }
+        
+        public float Armor { get; }
+
+
+        public Unit() : this(name: "Unknown Unit")
+        { }
+        public Unit(string name)
+        {
+            Name = name;
+            Damage = new Interval(0, 10);
+            Armor = 0.6f; 
+        }
+        public float GetRealHealth() {
+            return Health * (1f + Armor);
+                }
+        public bool SetDamage(float value)
+        {
+            _health -= value * Armor;
+            return _health <= 0f;
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Weapon
+    {
+        public string Name { get; }
+        private Interval Damage { get; }
+        public float Durability { get; }
+
+
+        public Weapon (string name)
+        {
+            Name = name;
+        }
+        public Weapon(string name, int minDamage, int maxDamage) : this(name: name) {
+            SetDamageParams(minDamage, maxDamage);
+                }
+        public void SetDamageParams(int minDamage, int maxDamage)
+        {
+            if (minDamage > maxDamage)
+            {
+                (minDamage, maxDamage) = (maxDamage, minDamage);
+                Console.WriteLine("incorrect input data: " + Name);
+            }
+            if (minDamage < 1)
+
+                if (maxDamage <= 1)
+                    maxDamage = 10;
+
+
+        }
+        public int GetDamage()
+        {
+            return (Damage.Min + Damage.Max) / 2;
+        }
+    }
+}
